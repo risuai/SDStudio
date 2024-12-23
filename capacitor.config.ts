c25d7c6bc91814b58ed2e0bc232dc62ad387df45
Yurly123 <joo7won@gmail.com>
2024-12-23 15:55:48 +0900

    Subtle change
    some modification, I don't sure it was a good idea

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'io.sunho.SDStudio',
   appName: 'SDStudio',
   webDir: 'release/app/dist/renderer',
-  server: {
-    url: "http://192.168.0.118:1212",
-    cleartext: true
-  },
   android: { allowMixedContent: true },
   plugins: {
     CapacitorHttp: {
